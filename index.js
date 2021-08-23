0e82985f8a3718277d3590d71c0e38275a696dc6
ADITYA VATSA <39759451+itscoolvatsa@users.noreply.github.com>
2021-08-23 23:02:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ app.use(express.json());
 app.use("/api/user", authRoute);
 app.use(templateRoute);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
     console.log("running on port 3000");
 });
